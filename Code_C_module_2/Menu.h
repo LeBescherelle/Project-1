6a92505407b6f514972cd36e901eed0722803ac4
Entwickel <44493820+Entwickel@users.noreply.github.com>
2018-11-19 16:05:15 +0100

    Add files via upload

--- Code_C_module_2/Menu.h
+++ Code_C_module_2/Menu.h
@@ -1,8 +1,13 @@
 #ifndef Menu
 #define Menu
 
-
+// mode normal , mode alterné et mode chenille
+void erase_fichier();
+void ecrire_fichier(int param);
+void ecrire_choix(int param);
 int afficher_menu();
+void demander_localisation();
+
 
 
 
--- Code_C_module_2/main.c
+++ Code_C_module_2/main.c
@@ -7,9 +7,11 @@
 
 int main()
 {
-    erase_fichier();              /*efface les données préexistence sur le fichier param.h */
-    int choix = afficher_menu();    /* affiche le menu et récupère la saisie de l'utilisateur dans la variable choix */     /* va définir ou sera stocker le fichier param.h */
-    ecrire_choix(choix);            /* va génerer le fichier param.h */
+    erase_fichier();
+    int choix = afficher_menu();
+     //efface les données préexistence sur le fichier param.h
+    demander_localisation(); // va définir ou sera stocker le fichier param.h
+    ecrire_choix(choix); // va génerer le fichier param.h
 
 }
 
